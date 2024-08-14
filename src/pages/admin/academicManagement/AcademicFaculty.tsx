2e147dc2c1f302508074130b002a2c4d261a04dd
programarreza <programarreza@gmail.com>
2024-08-14 23:36:56 +0600

    added to academic faculty

--- src/pages/admin/academicManagement/AcademicFaculty.tsx
+++ src/pages/admin/academicManagement/AcademicFaculty.tsx
@@ -1,8 +1,63 @@
+import { Button, Table, TableColumnsType, TableProps } from "antd";
+import { useGetAcademicFacultiesQuery } from "../../../redux/features/admin/academicManagement.api";
+import { TQueryParam } from "../../../types";
+import { TTableData } from "./AcademicSemester";
+
 const AcademicFaculty = () => {
+  const { data: facultyData, isFetching } =
+    useGetAcademicFacultiesQuery(undefined);
+
+  const tableData = facultyData?.data?.map(({ _id, name }) => ({
+    key: _id,
+    name,
+  }));
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      showSorterTooltip: { target: "full-header" },
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: () => {
+        return (
+          <div>
+            <Button>Update</Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  const onChange: TableProps<TTableData>["onChange"] = (
+    _pagination,
+    filters,
+    _sorter,
+    extra
+  ) => {
+    if (extra.action === "filter") {
+      const queryParams: TQueryParam[] = [];
+
+      filters.name?.forEach((item) =>
+        queryParams.push({ name: "name", value: item })
+      );
+
+      filters.year?.forEach((item) =>
+        queryParams.push({ name: "year", value: item })
+      );
+    }
+  };
+
   return (
-    <div>
-      <h2>AcademicFaculty Component Coming Soon</h2>
-    </div>
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+      showSorterTooltip={{ target: "sorter-icon" }}
+    />
   );
 };
 
--- src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
+++ src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
@@ -1,7 +1,49 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Col, Flex } from "antd";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import { toast } from "sonner";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+import { useAddAcademicFacultyMutation } from "../../../redux/features/admin/academicManagement.api";
+import { academicFacultySchema } from "../../../schemas/academicManagement.schema";
+
 const CreateAcademicFaculty = () => {
+  const [addAcademicFaculty] = useAddAcademicFacultyMutation();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Creating....");
+
+    const facultyData = {
+      name: data.name,
+    };
+
+    try {
+      const res = await addAcademicFaculty(facultyData);
+
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success(res.data.message, { id: toastId });
+      }
+    } catch (error) {
+      toast.error("Something went wrong ", { id: toastId });
+    }
+  };
+
   return (
     <div>
-      <h2>CreateAcademicFaculty Component Coming Soon</h2>
+      <Flex justify="center" align="center">
+        <Col span={6}>
+          <PHForm
+            onSubmit={onSubmit}
+            resolver={zodResolver(academicFacultySchema)}
+          >
+            <PHInput type={"text"} name={"name"} label={"Name: "} />
+
+            <Button htmlType="submit">Submit</Button>
+          </PHForm>
+        </Col>
+      </Flex>
     </div>
   );
 };
